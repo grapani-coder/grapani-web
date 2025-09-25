bbe222a736112c0aaa7fe1d75fef54bef4465022
Grapani <jeniffergrapani@gmail.com>
2025-09-25 12:00:28 +0800

    Created Login Form
    not finish the email id

---
+++ login.js
@@ -0,0 +1,19 @@
+function login(){
+
+    if (email==="jeniffergrapani@gmail.com") {
+         console.log("Your Email is Correct");
+    }else{
+        console.log("Your Email is Correct");  
+        document.getElementById("error").innerHTML = "Your Email is Incorrect.";
+
+    if (pass==="jeniffer_123") {
+         console.log("Your Password is Correct");
+
+     }else{
+        console.log("Your Password is Correct");  
+        document.getElementById("error").innerHTML = "Your Password is Incorrect.";
+
+        }
+    }
+
+ 
